68ded323f012b63ec2aeda7c62d61d51d9079da8
wangjiezhe <wangjiezhe@gmail.com>
2016-06-02 21:13:28 +0800

    clock: add control for chronograph

--- clock/main.py
+++ clock/main.py
@@ -22,12 +22,14 @@ LabelBase.register(
 class ClockApp(App):
 
     sw_seconds = 0
+    sw_started = False
 
     def update_time(self, nap):
         self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
         # self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
 
-        self.sw_seconds += nap
+        if self.sw_started:
+            self.sw_seconds += nap
         minutes, seconds = divmod(self.sw_seconds, 60)
         self.root.ids.stopwatch.text = (
             '{:02d}{:02d}.[size=40]{:02d}'.format(
@@ -38,6 +40,18 @@ class ClockApp(App):
 
     def on_start(self):
         Clock.schedule_interval(self.update_time, 0)
+
+    def start_stop(self):
+        self.root.ids.start_stop.text = (
+            'Start' if self.sw_started else 'Stop'
+        )
+        self.sw_started = not self.sw_started
+
+    def reset(self):
+        if self.sw_started:
+            self.root.ids.start_stop.text = 'Start'
+            self.sw_started = False
+        self.sw_seconds = 0
 
 
 def main():
