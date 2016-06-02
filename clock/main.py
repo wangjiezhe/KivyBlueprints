c01f9f0fc477915a93a190ff8f02a75a56c1e27b
wangjiezhe <wangjiezhe@gmail.com>
2016-06-02 21:02:33 +0800

    clock: show chronograph

--- clock/main.py
+++ clock/main.py
@@ -21,12 +21,23 @@ LabelBase.register(
 
 class ClockApp(App):
 
+    sw_seconds = 0
+
     def update_time(self, nap):
         self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
         # self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
 
+        self.sw_seconds += nap
+        minutes, seconds = divmod(self.sw_seconds, 60)
+        self.root.ids.stopwatch.text = (
+            '{:02d}{:02d}.[size=40]{:02d}'.format(
+                int(minutes), int(seconds),
+                int(seconds * 100 % 100)
+            )
+        )
+
     def on_start(self):
-        Clock.schedule_interval(self.update_time, 1)
+        Clock.schedule_interval(self.update_time, 0)
 
 
 def main():
