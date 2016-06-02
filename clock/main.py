b564fc9d56d7e71319a1659bb2b0cfc646671843
wangjiezhe <wangjiezhe@gmail.com>
2016-06-02 19:29:21 +0800

    clock: show time

--- clock/main.py
+++ clock/main.py
@@ -1,9 +1,12 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.text import LabelBase
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
+# from kivy.properties import ObjectProperty
+# from kivy.uix.boxlayout import BoxLayout
 
-Window.clearcolor = get_color_from_hex('#101216')
+from time import strftime
 
 LabelBase.register(
     name='Roboto',
@@ -12,9 +15,24 @@ LabelBase.register(
 )
 
 
+# class ClockLayout(BoxLayout):
+#     time_prop = ObjectProperty(None)
+
+
 class ClockApp(App):
-    pass
+
+    def update_time(self, nap):
+        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+        # self.root.time_prop.text = strftime('[b]%H[/b]:%M:%S')
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
+
+
+def main():
+    Window.clearcolor = get_color_from_hex('#101216')
+    ClockApp().run()
 
 
 if __name__ == '__main__':
-    ClockApp().run()
+    main()
