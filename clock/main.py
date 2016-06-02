d695bd551636d5d2e7d45b9518ec116a5eb59069
wangjiezhe <wangjiezhe@gmail.com>
2016-06-02 18:54:43 +0800

    clock: new UI

--- clock/main.py
+++ clock/main.py
@@ -1,4 +1,15 @@
 from kivy.app import App
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
+from kivy.utils import get_color_from_hex
+
+Window.clearcolor = get_color_from_hex('#101216')
+
+LabelBase.register(
+    name='Roboto',
+    fn_regular='fonts/Roboto-Thin.ttf',
+    fn_bold='fonts/Roboto-Medium.ttf'
+)
 
 
 class ClockApp(App):
