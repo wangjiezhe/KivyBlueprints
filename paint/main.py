d43d5681d2625e5156623837a67130be8c4e1883
wangjiezhe <wangjiezhe@gmail.com>
2016-06-04 14:14:11 +0800

    paint: better ui

--- paint/main.py
+++ paint/main.py
@@ -1,5 +1,36 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.base import EventLoop
+from kivy.utils import get_color_from_hex
+from kivy.config import Config
+
+
+CURSOR = (
+    '       @@@@             ',
+    '       @--@             ',
+    '       @--@             ',
+    '       @--@             ',
+    '       @--@             ',
+    '       @@@@             ',
+    '                        ',
+    '@@@@@@ @@@@ @@@@@@      ',
+    '@----@ @--@ @----@      ',
+    '@----@ @--@ @----@      ',
+    '@@@@@@ @@@@ @@@@@@      ',
+    '                        ',
+    '       @@@@             ',
+    '       @--@             ',
+    '       @--@             ',
+    '       @--@             ',
+    '       @--@             ',
+    '       @@@@             ',
+    '                        ',
+    '                        ',
+    '                        ',
+    '                        ',
+    '                        ',
+    '                        ',
+)
 
 
 class CanvasWidget(Widget):
@@ -9,10 +40,28 @@ class CanvasWidget(Widget):
 class PaintApp(App):
 
     def build(self):
+        EventLoop.ensure_window()
+        if EventLoop.window.__class__.__name__.endswith('Pygame'):
+            try:
+                from pygame import mouse
+                from pygame import cursors
+                a, b = cursors.compile(CURSOR, black='@', white='-')
+                mouse.set_cursor((24, 24), (9, 9), a, b)
+            except:
+                pass
         return CanvasWidget()
 
 
 def main():
+    Config.set('graphics', 'width', '960')
+    Config.set('graphics', 'height', '540')
+    # Config.set('graphics', 'resizable', '0')
+    # Config.set('input', 'mouse', 'mouse,disable_multitouch')
+
+    from kivy.core.window import Window
+
+    Window.clearcolor = get_color_from_hex('#FFFFFF')
+
     PaintApp().run()
 
 
